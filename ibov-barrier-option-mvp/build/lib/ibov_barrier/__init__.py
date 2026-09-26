567af7b1018a379588e410939c1b191162695c28
luiz.henrique.felipe.rocha@gmail.com <luiz.henrique.felipe.rocha@gmail.com>
2026-09-26 04:41:43 +0000

    chore: untrack data/ and build/ directories

--- ibov-barrier-option-mvp/build/lib/ibov_barrier/__init__.py
+++
@@ -1,4 +0,0 @@
-from .pricing import BarrierContract, MarketData, PriceResult, greeks, price_down_and_out_call
-
-__all__ = ["BarrierContract", "MarketData", "PriceResult", "greeks", "price_down_and_out_call"]
-
